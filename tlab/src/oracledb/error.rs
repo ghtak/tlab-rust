0585e9d4a3970a5da0b6e7ee941c01bd8c7ee653
gwonhyeong.tak <gwonhyeong.tak@gmail.com>
2026-08-26 23:44:47 +0900

    feat: add database pool implementation and error mapping for Oracle support

--- tlab/src/oracledb/error.rs
+++ tlab/src/oracledb/error.rs
@@ -1,26 +1,7 @@
 use crate::db::Error;
 
 pub(crate) fn map_error(error: impl std::error::Error + Send + Sync + 'static) -> Error {
-    let source = anyhow::Error::new(error);
-
-    if matches!(
-        source.downcast_ref::<oracle_rs::Error>(),
-        Some(oracle_rs::Error::OracleError { code: 1, .. })
-    ) {
-        Error::UniqueViolation { source }
-    } else {
-        Error::Driver { source }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn maps_oracle_unique_constraint_errors_to_unique_violations() {
-        let error = map_error(oracle_rs::Error::oracle(1, "unique constraint violated"));
-
-        assert!(matches!(error, Error::UniqueViolation { .. }));
+    Error::Driver {
+        source: anyhow::Error::new(error),
     }
 }
